97ff32e4406db4e786b3ea609234fe5a4aa113b0
cris93g <37388115+cris93g@users.noreply.github.com>
2019-01-24 16:28:22 -0600

    started my built portfolio

---
+++ src/config/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+	siteTitle: "Cristian Gonzalez | Software Engineer",
+	siteDescription: "",
+	siteUrl: "",
+	siteImage: "",
+	name: "",
+	location: "",
+	email: "",
+	socialMedia: [
+		{
+			name: "Github",
+			url: ""
+		},
+		{
+			name: "Linkedin",
+			url: ""
+		},
+		{ name: "Instagram", url: "" },
+		{
+			name: "twitter",
+			url: ""
+		}
+	],
+	nav: [
+		{
+			name: "About",
+			url: "#about"
+		},
+		{
+			name: "Experience",
+			url: "#jobs"
+		},
+		{
+			name: "Work",
+			url: "#projects"
+		},
+		{
+			name: "Contact",
+			url: "#contact"
+		}
+	]
+};
